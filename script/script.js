d78d3aafa17e2ac30ba0aea08d4cd7a0920006e5
CHOKOLADis <igor2011__igor@mail.ru>
2020-09-11 17:58:27 +0500

    v1.2

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$('.cards').slick({
+    dots: true,
+    arrow:false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed:300,
+    autoplay:true,
+    autoplaySpeed:2000,
+    centerMode: true,
+    variableWidth: true
+});
